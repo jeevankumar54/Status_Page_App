e79b726934d309c4f52403e89d8a2a769d1c9c4c
jeevankumar54 <tjeevankumar54@gmail.com>
2025-04-11 14:22:55 +0530

    Update backend files

--- backend/app/models/team.py
+++ backend/app/models/team.py
@@ -1,29 +1,16 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.sql import func
-import enum
 
 from app.db.base_class import Base
+from app.models.user import user_team
 
 
-class ServiceStatus(str, enum.Enum):
-    OPERATIONAL = "operational"
-    DEGRADED_PERFORMANCE = "degraded_performance"
-    PARTIAL_OUTAGE = "partial_outage"
-    MAJOR_OUTAGE = "major_outage"
-    MAINTENANCE = "maintenance"
-
-
-class Service(Base):
+class Team(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True, nullable=False)
     description = Column(String(255), nullable=True)
-    status = Column(
-        Enum(ServiceStatus), 
-        default=ServiceStatus.OPERATIONAL, 
-        nullable=False
-    )
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
@@ -31,5 +18,5 @@ class Service(Base):
     organization_id = Column(Integer, ForeignKey("organization.id"))
     
     # Relationships
-    organization = relationship("Organization", back_populates="services")
-    incidents = relationship("Incident", secondary="incident_service")
+    organization = relationship("Organization", back_populates="teams")
+    users = relationship("User", secondary=user_team, back_populates="teams")
